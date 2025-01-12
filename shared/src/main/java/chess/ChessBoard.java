172dd9a2caf0bc875308903b413df118295c3833
James Teuscher <james.teuscher@outlook.com>
2025-01-11 18:15:57 -0700

    Update ChessBoard.java, partial resetBoard()

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -35,13 +35,51 @@ public class ChessBoard {
         return this.piecesOnBoard.get(position);
     }
 
+    private void resetHelper(ChessGame.TeamColor teamColor) {
+        if (teamColor == ChessGame.TeamColor.WHITE) {
+            this.piecesOnBoard.put( new ChessPosition(1,1), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
+            this.piecesOnBoard.put(new ChessPosition(1,2), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
+            this.piecesOnBoard.put(new ChessPosition(1,3), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
+            this.piecesOnBoard.put(new ChessPosition(1,4), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN));
+            this.piecesOnBoard.put(new ChessPosition(1,5), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING));
+            this.piecesOnBoard.put(new ChessPosition(1,6), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
+            this.piecesOnBoard.put(new ChessPosition(1,7), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
+            this.piecesOnBoard.put( new ChessPosition(1,8), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
+            for (int i = 1; i < 9; i++){
+                this.piecesOnBoard.put(new ChessPosition(2, i), new ChessPiece(teamColor, ChessPiece.PieceType.PAWN));
+            }
+        } else{
+            this.piecesOnBoard.put( new ChessPosition(8,1), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
+            this.piecesOnBoard.put(new ChessPosition(8,2), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT));
+            this.piecesOnBoard.put(new ChessPosition(8,3), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
+            this.piecesOnBoard.put(new ChessPosition(8,4), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN));
+            this.piecesOnBoard.put(new ChessPosition(8,5), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING));
+            this.piecesOnBoard.put(new ChessPosition(8,6), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
+            this.piecesOnBoard.put(new ChessPosition(8,7), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT));
+            this.piecesOnBoard.put( new ChessPosition(8,8), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
+            for (int i = 1; i < 9; i++){
+                this.piecesOnBoard.put(new ChessPosition(7, i), new ChessPiece(teamColor, ChessPiece.PieceType.PAWN));
+            }
+        }
+
+    }
+
     /**
      * Sets the board to the default starting board
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
+        this.piecesOnBoard.clear();
         // create hashmap that contains the corresponding pieces at their starting positions on each side of the board
+        resetHelper(ChessGame.TeamColor.BLACK);
+        resetHelper(ChessGame.TeamColor.WHITE);
         // set `piecesOnBoard` field to that hashmap
-        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public String toString() {
+        return "ChessBoard{" +
+                "piecesOnBoard=" + piecesOnBoard +
+                '}';
     }
 }
