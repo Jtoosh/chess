a611ff58923b2f4db7efca4399f950c3afbeda89
James Teuscher <james.teuscher@outlook.com>
2025-01-14 19:56:22 -0700

    Update PieceMoveCalc interface; Add RookMoveCalc

--- shared/src/main/java/chess/PieceMoveCalc.java
+++ shared/src/main/java/chess/PieceMoveCalc.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Collection;
+
 public interface PieceMoveCalc {
+  public Collection<ChessMove> pieceMoves (ChessBoard board, ChessPosition position);
 
 }
---
+++ shared/src/main/java/chess/RookMoveCalc.java
@@ -0,0 +1,11 @@
+package chess;
+
+import java.util.Collection;
+
+public class RookMoveCalc implements PieceMoveCalc{
+
+  @Override
+  public Collection<ChessMove> pieceMoves (ChessBoard board, ChessPosition position){
+    return null;
+  }
+}
