2c6ac46c4c14b837116c7874d1561073a00e97f3
James Teuscher <james.teuscher@outlook.com>
2025-01-24 19:50:28 -0700

    Update PawnMoveCalc with progress

--- shared/src/main/java/chess/PawnMoveCalc.java
+++ shared/src/main/java/chess/PawnMoveCalc.java
@@ -11,54 +11,47 @@ public class PawnMoveCalc implements PieceMoveCalc {
   private final ChessPiece pawnPiece;
   private final ChessPosition pawnPosition;
   private boolean initialMove;
+  private int direction;
 
   public PawnMoveCalc(ChessBoard currentBoard, ChessPosition piecePosition) {
     this.currentBoardPieces=currentBoard.getBoardPieces();
     this.pawnPosition=piecePosition;
     this.pawnPiece=currentBoardPieces[pawnPosition.getRow() - 1][pawnPosition.getColumn() - 1];
-    this.initialMove=pawnPosition.getRow() == 2 || this.pawnPosition.getRow() == 7;
+    this.initialMove=(pawnPosition.getRow() == 2 && pawnPiece.getTeamColor() == ChessGame.TeamColor.WHITE) || (this.pawnPosition.getRow() == 7&& pawnPiece.getTeamColor() == ChessGame.TeamColor.BLACK);
+    this.direction = (pawnPiece.getTeamColor() == ChessGame.TeamColor.WHITE) ? 1 : -1;
   }
 
   @Override
   public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
-    if (this.initialMove) {
-      checkInitialMove();
+    if(currentBoardPieces[(pawnPosition.getRow()-1) + (direction)][(pawnPosition.getColumn()-1)] == null){
+      ChessPosition newPosition = new ChessPosition(pawnPosition.getRow() + 1, pawnPosition.getColumn());
+      validMovePositions.add(new ChessMove(pawnPosition,newPosition, null ));
     }
-    return List.of();
-  }
 
-  private void checkInitialMove() {
-    if (currentBoardPieces[(pawnPosition.getRow() - 1) + 1][(pawnPosition.getColumn() - 1)] == null) {
-      ChessPosition newPosition=new ChessPosition(pawnPosition.getRow() + 1, pawnPosition.getColumn());
-      validMovePositions.add(new ChessMove(pawnPosition, newPosition, null));
-      this.initialMove=false;
-    }
-    if (currentBoardPieces[(pawnPosition.getRow() - 1) + 2][(pawnPosition.getColumn() - 1)] == null) {
-      ChessPosition newPosition=new ChessPosition(pawnPosition.getRow() + 2, pawnPosition.getColumn());
-      validMovePositions.add(new ChessMove(pawnPosition, newPosition, null));
-      this.initialMove=false;
-    }
-  }
-
-  private void checkCaptureMove(){
-    ChessPiece leftCaptureSqaure = currentBoardPieces[(pawnPosition.getRow() - 1) + 1][(pawnPosition.getColumn() - 1)-1];
-    ChessPiece rightCaptureSqaure = currentBoardPieces[(pawnPosition.getRow() - 1) + 1][(pawnPosition.getColumn() - 1)+1];
-    ChessPiece forwardSquare = currentBoardPieces[(pawnPosition.getRow() - 1) + 1][(pawnPosition.getColumn() - 1)];
-    ChessPiece [] potentialMoves = {leftCaptureSqaure, rightCaptureSqaure, forwardSquare};
-    
-    if (leftCaptureSqaure == null && rightCaptureSqaure == null){
-      return;
-    }
-    for (ChessPiece square : potentialMoves){
-      if (square == null){
-        break;
-      } else{
-        ChessPosition newPosition=new ChessPosition();
+    if ((pawnPosition.getColumn() - 1) > 0){
+      ChessPiece potentialCaptureLeft=currentBoardPieces[(pawnPosition.getRow() - 1) + direction][(pawnPosition.getColumn() - 1) - 1];
+      if (potentialCaptureLeft != null && !potentialCaptureLeft.teamColorsEqual(pawnPiece)) {
+        ChessPosition newPosition=new ChessPosition(pawnPosition.getRow() + 1, pawnPosition.getColumn() - 1);
+        validMovePositions.add(new ChessMove(pawnPosition, newPosition, null));
       }
     }
-
-
+    if ((pawnPosition.getColumn() - 1) < 7){
+      ChessPiece potentialCaptureRight=currentBoardPieces[(pawnPosition.getRow() - 1) + direction][(pawnPosition.getColumn() - 1) + 1];
+      if (potentialCaptureRight != null && !potentialCaptureRight.teamColorsEqual(pawnPiece)) {
+        ChessPosition newPosition=new ChessPosition(pawnPosition.getRow() + 1, pawnPosition.getColumn() + 1);
+        validMovePositions.add(new ChessMove(pawnPosition, newPosition, null));
+      }
+    }
+    if (initialMove && currentBoardPieces[(pawnPosition.getRow()-1) + (2 * direction)][(pawnPosition.getColumn()-1)] == null){
+      ChessPosition newPosition = new ChessPosition(pawnPosition.getRow() + (2 * direction), pawnPosition.getColumn());
+      validMovePositions.add(new ChessMove(pawnPosition,newPosition, null ));
+    }
+    return validMovePositions;
   }
+
+
+
+
 
 }
 
