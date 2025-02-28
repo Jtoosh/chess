6310041cdc47c3cc5c5c698a4c2a44f747d00b24
James Teuscher <james.teuscher@outlook.com>
2025-02-27 21:32:04 -0700

    Clean up, IntelliJ Code inspection results

---
+++ server/src/test/java/service/ListTests.java
@@ -0,0 +1,13 @@
+package service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ListGamesTests {
+
+  @Test
+  @DisplayName("Positive")
+  void listGamesTest(){
+
+  }
+}
