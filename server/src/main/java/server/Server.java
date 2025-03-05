db68dcd0cf34de88c61c542117fd6b16a2b65505
James Teuscher <james.teuscher@outlook.com>
2025-03-04 20:17:32 -0700

    Update Server.java, cleaner run method

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -21,93 +21,70 @@ public class Server {
     private final CreateService createService = new CreateService(authDataAccess, gameDataAccess);
     private final JoinService joinService = new JoinService(userDataAccess, authDataAccess, gameDataAccess);
 
+    private void handleResponse(Response res, Record request, ServiceHandler service){
+      try{
+        Record response = service.executeService(request);
+        res.status(200);
+        res.body(serializer.toJSON(response));
+      } catch (IllegalArgumentException e){
+        res.status(400);
+        res.body(serializer.toJSON(new ErrorResponse(e.getMessage())));
+      } catch (AuthorizationException e) {
+        res.status(401);
+        res.body(serializer.toJSON(new ErrorResponse(e.getMessage())));
+      } catch (AlreadyInUseException e){
+        res.status(403);
+        res.body(serializer.toJSON(new ErrorResponse(e.getMessage())));
+      } catch (Exception e) {
+        res.status(500);
+        res.body(serializer.toJSON(new ErrorResponse(e.getMessage())));
+      }
+    }
+
     public int run(int desiredPort) {
         Spark.port(desiredPort);
 
         Spark.staticFiles.location("web");
-
         // Register your endpoints and handle exceptions here.
+
         //Clear Endpoint
         Spark.delete("/db", (req, res) ->{
-          ClearResponse response =null;
-          try {
-            response=clearService.clear();
-            res.status(200);
-            res.body("");
-          } catch (Exception e) {
-            res.status(500);
-            res.body(serializer.toJSON(new ErrorResponse(e.getMessage())));
-          }
+          ClearRequest clearRequest = new ClearRequest(req.headers(), req.body());
+          ServiceHandler clearServiceLambda = request -> clearService.clear();
+
+          handleResponse(res, clearRequest, clearServiceLambda );
           return res.body();
         });
 
         //Register Endpoint
         Spark.post("/user", (req, res) ->{
-            RegisterRequest registerRequest = serializer.fromJSON(req.body(), RegisterRequest.class);
-          RegisterResponse response =null;
-          try {
-            response=registerService.register(registerRequest);
-              res.status(200);
-            res.body(serializer.toJSON(response));
-          } catch (AuthorizationException e) {
-            res.status(401);
-            res.body(serializer.toJSON(new ErrorResponse(e.getMessage())));
-          } catch (AlreadyInUseException e){
-            res.status(403);
-            res.body(serializer.toJSON(new ErrorResponse(e.getMessage())));
-          } catch (IllegalArgumentException e){
-            res.status(400);
-            res.body(serializer.toJSON(new ErrorResponse(e.getMessage())));
-          }
-
-            return res.body();
+          RegisterRequest registerRequest = serializer.fromJSON(req.body(), RegisterRequest.class);
+          ServiceHandler registerServiceLambda = request -> registerService.register((RegisterRequest) request);
+          handleResponse(res, registerRequest, registerServiceLambda);
+          return res.body();
         });
 
         //Login Endpoint
         Spark.post("/session", (req, res) ->{
             LoginRequest loginRequest = serializer.fromJSON(req.body(), LoginRequest.class);
-          LoginResponse response =null;
-          try {
-            response=loginService.login(loginRequest);
-            res.status(200);
-            res.body(serializer.toJSON(response));
-          } catch (AuthorizationException e) {
-            res.status(401);
-            res.body(serializer.toJSON(new ErrorResponse(e.getMessage())));
-          } catch (Exception e){
-            res.status(500);
-            res.body(serializer.toJSON(new ErrorResponse(e.getMessage())));
-          }
+            ServiceHandler loginServiceLambda = request -> loginService.login((LoginRequest)request);
+            handleResponse(res, loginRequest, loginServiceLambda);
             return res.body();
         });
 
         //Logout endpoint
         Spark.delete("/session", (req, res) ->{
          LogoutRequest logoutRequest = new LogoutRequest(req.headers("Authorization"));
-          LogoutResponse response =null;
-          try {
-            response=logoutService.logout(logoutRequest);
-            res.status(200);
-            res.body("");
-          } catch (AuthorizationException e) {
-            res.status(401);
-            res.body(serializer.toJSON(new ErrorResponse(e.getMessage())));
-          }
+         ServiceHandler logoutServiceLambda = request -> logoutService.logout((LogoutRequest) request);
+         handleResponse(res, logoutRequest, logoutServiceLambda);
          return res.body();
         });
 
         //List Games endpoint
         Spark.get("/game", (req, res) ->{
             ListRequest listRequest = new ListRequest(req.headers("Authorization"));
-          try {
-            ListResponse response = listService.listGames(listRequest);
-            res.status(200);
-            res.body(serializer.toJSON(response));
-          } catch (AuthorizationException e) {
-            res.status(401);
-            res.body(serializer.toJSON(new ErrorResponse(e.getMessage())));
-          }
-
+            ServiceHandler listServiceLambda = request -> listService.listGames((ListRequest) request);
+            handleResponse(res, listRequest, listServiceLambda);
           return res.body();
         });
 
@@ -115,14 +92,8 @@ public class Server {
         Spark.post("/game", (req, res) -> {
           CreateRequest testRequest = serializer.fromJSON(req.body(), CreateRequest.class);
           CreateRequest createRequest = new CreateRequest(req.headers("Authorization"), testRequest.gameName());
-          try{
-            CreateResponse response = createService.createGame((createRequest));
-            res.status(200);
-            res.body(serializer.toJSON(response));
-          } catch (AuthorizationException e) {
-            res.status(401);
-            res.body(serializer.toJSON(new ErrorResponse(e.getMessage())));
-          }
+          ServiceHandler createServiceLambda = request -> createService.createGame((CreateRequest) request);
+          handleResponse(res, createRequest, createServiceLambda);
           return res.body();
         });
 
@@ -130,20 +101,8 @@ public class Server {
         Spark.put("/game", (req,res)->{
           JoinRequest tempRequest = serializer.fromJSON(req.body(), JoinRequest.class);
           JoinRequest joinRequest = new JoinRequest(req.headers("Authorization"), tempRequest.playerColor(), tempRequest.gameID());
-          try{
-            JoinResponse response = joinService.joinGame(joinRequest);
-            res.status(200);
-            res.body("");
-          } catch (AuthorizationException e) {
-            res.status(401);
-            res.body(serializer.toJSON(new ErrorResponse(e.getMessage())));
-          } catch (IllegalArgumentException e) {
-            res.status(400);
-            res.body(serializer.toJSON(new ErrorResponse(e.getMessage())));
-          } catch (AlreadyInUseException e) {
-            res.status(403);
-            res.body(serializer.toJSON(new ErrorResponse(e.getMessage())));
-          }
+          ServiceHandler joinServiceLambda = request -> joinService.joinGame((JoinRequest) request);
+          handleResponse(res, joinRequest, joinServiceLambda);
           return res.body();
         });
 
@@ -159,7 +118,5 @@ public class Server {
         Spark.awaitStop();
     }
 
-    private void handleResponse(Record response){
 
-    }
 }
---
+++ server/src/main/java/service/ServiceHandler.java
@@ -0,0 +1,5 @@
+package service;
+
+public interface ServiceHandler {
+  Record executeService(Record request);
+}
