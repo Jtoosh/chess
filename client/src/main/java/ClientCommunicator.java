a3c65de317fb6970a3dbcd0a785091013fd89932
James Teuscher <james.teuscher@outlook.com>
2025-03-23 21:48:19 -0600

    Add ClientCommunicator.java; add POST method

--- client/src/main/java/ClientCommunicator.java
+++ client/src/main/java/ClientCommunicator.java
@@ -1,6 +1,7 @@
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -17,6 +18,29 @@ public class ClientCommunicator {
         connection.connect();
 
         if (connection.getResponseCode() == HttpURLConnection.HTTP_OK){
+            InputStream responseBody = connection.getInputStream();
+            BufferedInputStream responseBodyOptimized = new BufferedInputStream(responseBody);
+        } else{
+            InputStream errorStream = connection.getErrorStream();
+        }
+    }
+
+    public void httpPOST(String urlArg) throws IOException{
+        URL connectionURL = new URL(urlArg);
+
+        HttpURLConnection connection = (HttpURLConnection) connectionURL.openConnection();
+
+        connection.setReadTimeout(5000);
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+
+        connection.connect();
+
+        try(OutputStream requestBody = connection.getOutputStream();) {
+            // Write request body to OutputStream ...
+        }
+
+        if (connection.getResponseCode() == HttpURLConnection.HTTP_SEE_OTHER){
             InputStream responseBody = connection.getInputStream();
             BufferedInputStream responseBodyOptimized = new BufferedInputStream(responseBody);
         } else{
