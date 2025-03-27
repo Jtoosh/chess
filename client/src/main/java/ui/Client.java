31a77f7e566c304f1c6b76173f994bf1caf4ec40
James Teuscher <james.teuscher@outlook.com>
2025-03-26 20:15:24 -0600

    Remove duplicate code

--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -1,11 +1,6 @@
 package ui;
 
-import client.AlreadyInUseException;
-import client.AuthorizationException;
-import client.HttpHandler;
 import client.ServerFacade;
-
-import java.io.IOException;
 
 public class Client {
     private final ServerFacade serverFacade = new ServerFacade(8080);
@@ -16,19 +11,5 @@ public class Client {
 
         System.out.println(MenuStrings.PRELOGIN_MENU);
         menuDrawer.repl(state);
-    }
-
-    private Record handleResponse(HttpHandler commHandler) throws IOException {
-        try{
-            return commHandler.makeRequest();
-        } catch (IllegalArgumentException e){
-            throw new IllegalArgumentException(e);
-        } catch (AuthorizationException e){
-            throw new AuthorizationException(e.getMessage());
-        } catch (AlreadyInUseException e) {
-            throw new AlreadyInUseException(e.getMessage());
-        } catch (IOException e){
-            throw new IOException(e);
-        }
     }
 }
